5ffa8c018a278bb735b1b5332a0789df8c727be5
Tanyas98 <tanyasoerensen98@gmail.com>
2020-09-08 12:32:21 +0200

    upload solution

---
+++ Cars.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_OOP_cars_lister
+{
+    class Cars
+    {
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string model;
+
+		public string Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		private string brand;
+
+		public string Brand
+		{
+			get { return brand; }
+			set { brand = value; }
+		}
+
+		private int year;
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+
+		private int price;
+
+		public int Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+		private string color;
+
+		public string Color
+		{
+			get { return color; }
+			set { color = value; }
+		}
+
+		public Cars(int ID, string bilmodel, string mærke, int år, int pris, string farve)
+		{
+			Id = ID;
+			Model = bilmodel;
+			Brand = mærke;
+			Year = år;
+			Price = pris;
+			Color = farve;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H1_OOP_cars_lister
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cars> cars = new List<Cars>
+            {
+                new Cars(1, "Rio", "Kia", 2006, 250000, "Red"),
+                new Cars(2, "Rio", "Kia", 2007, 265000, "Black"),
+                new Cars(3, "Panda", "Fiat", 2017, 256000, "Black"),
+                new Cars(4, "Panda", "Fiat", 2017, 365000, "Yellow"),
+                new Cars(5, "S40", "Volvo", 2018, 665000, "White"),
+                new Cars(6, "S65", "Volvo", 2018, 600000, "White"),
+                new Cars(7, "Gorm", "Ferrari", 2000, 4665000, "White"),
+                new Cars(8, "Gorm", "Ferrari", 2018, 4665000, "White"),
+            };
+            List<string> nyListe = listTal.Where(number => number <= 0).ToList();
+        }
+    }
+}
